fabfdaf5b10a6922b2c944b447c4dbb9ad137f16
谢余锋 <576562848@qq.com>
2017-06-24 15:02:54 +0800

    审核审批

---
+++ src/common/models/verify-record.ts
@@ -0,0 +1,16 @@
+export interface VerifyRecord {
+  id?: string;
+  category: string;
+  batchNumber: string;
+  businessId: string;
+  businessName: string;
+  stageBeforeVerify: number;
+  stageAfterVerify: number;
+  verifyStatus: boolean;
+  applyPerson: string;
+  applyTime: Date;
+  verifyPerson: string;
+  verifyTime: Date;
+  remark: string;
+  orgId: string;
+}
