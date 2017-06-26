9d8dcf28ea545d1e47e476ae96e940b110286a2a
谢余锋 <576562848@qq.com>
2017-06-26 16:51:23 +0800

    审批审核记录

--- src/common/models/verify-record.ts
+++ src/common/models/verify-record.ts
@@ -1,16 +1,20 @@
 export interface VerifyRecord {
   id?: string;
-  category: string;
+  category: number;
   batchNumber: string;
   businessId: string;
+  businessType: number;
+  businessTypeStr: string;
   businessName: string;
   stageBeforeVerify: number;
   stageAfterVerify: number;
-  verifyStatus: boolean;
-  applyPerson: string;
-  applyTime: Date;
-  verifyPerson: string;
-  verifyTime: Date;
+  verifyStatus: number;
+  createAccount: string;
+  createTime: Date;
+  createTimeStr: string;
+  modifyAccount: string;
+  modifyTime: Date;
+  modifyTimeStr: string;
   remark: string;
   orgId: string;
 }
@@ -18,6 +22,7 @@ export interface VerifyRecord {
 export interface VerifyBusiness {
   batchNumber: string;
   businessId: string;
+  businessType: string;
   businessName: string;
   stageBeforeVerify: number;
 }
