07b662ceaa3cc593dce719ae7d8495b65776726d
谢余锋 <576562848@qq.com>
2017-06-26 13:48:46 +0800

    添加moment组件

--- config.js
+++ config.js
@@ -30,6 +30,7 @@ System.config({
     "bootstrap-datepicker": "npm:bootstrap-datepicker@1.6.4",
     "font-awesome": "npm:font-awesome@4.7.0",
     "jquery": "npm:jquery@3.2.1",
+    "moment": "npm:moment@2.18.1",
     "nprogress": "github:rstacruz/nprogress@0.2.0",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.4.1"
