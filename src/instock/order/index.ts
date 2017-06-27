7123426c873221be94c26b0573a38322c10d15ee
sunshun <ssnoodles0226@gmail.com>
2017-06-27 16:03:46 +0800

    入库单

---
+++ src/instock/order/index.ts
@@ -0,0 +1,13 @@
+import { Router, RouterConfiguration } from "aurelia-router";
+
+export class InstockOrderRouter {
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.map([
+      { route: '', name: 'list', moduleId: './list' },
+      { route: '/:id/view', name: 'view', moduleId: './view', title: '查看' }
+    ]);
+    this.router = router;
+  }
+}
