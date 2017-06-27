98b67f471031481c13c693695e4f4754192b5ae5
谢余锋 <576562848@qq.com>
2017-06-27 14:33:38 +0800

    代码规范

--- src/base/models/contract.ts
+++ src/base/models/contract.ts
@@ -1,9 +1,11 @@
 export interface Contract {
-  id: string ;
+  id: string;
   /**
    * 1：客户仓储；2：装卸单位；3：库区租赁
    */
-  contractType: number ;
+  contractType: number;
+  
+  contractTypeStr: string;
 
   customerId: string;
 
