9d2153ffa943fbb3686de1606d83c78ae7e96b4c
谢余锋 <576562848@qq.com>
2017-06-27 14:33:38 +0800

    代码规范

--- src/base/contract/list.ts
+++ src/base/contract/list.ts
@@ -13,20 +13,20 @@ export class ContractList {
         pageSizes: true,
         buttonCount: 10
     };
+    contractTypes: string[] = ["客户仓储", "装卸单位", "库区租赁"];
 
     constructor(private contractService: ContractService,
         private messageDialogService: MessageDialogService,
         private dataSourceFactory: DataSourceFactory) {
         this.dataSource = this.dataSourceFactory.create({
-            query: () => this.contractService.queryContracts({ searchName: this.searchName }),
+            query: () => this.contractService.queryContracts({searchName: this.searchName}).map(res => {
+                res.contractTypeStr = this.contractTypes[res.contractType - 1];
+                return res;
+            }),
             pageSize: 10
         });
     }
-
-    formatMethod(type: number) {
-        return ['客户仓储', '装卸单位', '库区租赁', 'delete'][type - 1] || 'unknown';
-    }
-
+    
     async delete(id) {
         let confirm = await this.messageDialogService.confirm({ title: "提示", message: "确定删除该角色！" });
         if (confirm) {
