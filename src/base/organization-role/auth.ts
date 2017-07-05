e44dae33fc5263c22407a2069896cabe8ff325ae
sunshun <ssnoodles0226@gmail.com>
2017-07-05 11:00:30 +0800

    修改角色授权菜单

--- src/base/organization-role/auth.ts
+++ src/base/organization-role/auth.ts
@@ -6,13 +6,14 @@ import { inject} from "aurelia-dependency-injection";
 import { DialogService } from "ui";
 import { OrganizationRoleService} from "@app/base/services/organization-role";
 import { Menu} from "@app/base/models/menu";
+import { OrganizationRole } from "@app/base/models/organization-role";
 
 export class AuthMenu {
 
   id: string;
-  menuItems: Menu[];
-  assignedMenuItems: Menu[];
-
+  menuItems: Menu[] = [];
+  assignedMenuItems: Menu[] = [];
+  orgRole: OrganizationRole;
   constructor(@inject private router: Router,
               @inject private organizationRoleService: OrganizationRoleService,
               @inject private dialogService: DialogService) {
@@ -20,9 +21,11 @@ export class AuthMenu {
 
   async activate(params) {
     this.id = params.id;
-    this.menuItems = await this.organizationRoleService.getOrgMenu();
-    this.assignedMenuItems = await this.organizationRoleService.getOrgRoleMenu(params.id);
-
+    this.orgRole = await this.organizationRoleService.getOrgRole(params.id);
+    let menus = await this.organizationRoleService.getOrgMenu();
+    if (menus.length != 0) this.menuItems = menus;
+    let assignedMenus = await this.organizationRoleService.getOrgRoleMenu(params.id);
+    if (assignedMenus.length != 0) this.assignedMenuItems = assignedMenus;
   }
 
   async assignOrgRoleMenu() {
@@ -39,5 +42,20 @@ export class AuthMenu {
   cancel() {
     this.router.navigateToRoute("list");
   }
+
+  /* tslint:disable */
+  getIcon(type: number, parentId: any) {
+    if (type == 0) {
+      return "fa fa-list-alt text-primary";
+    } else if (type == 1) {
+      return "fa fa-bolt  text-warning";
+    } else if (type == -1 && !parentId) {
+      return "fa fa-window-maximize text-info";
+    } else if (type == -1 && parentId) {
+      return "fa fa-window-restore text-info";
+    }
+    return null;
+  }
+  /* tslint:enable */
 
 }
