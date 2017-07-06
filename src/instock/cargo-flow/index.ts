0a0bb6387997b3c33f8d6c07219119e6994b80b3
谢余锋 <576562848@qq.com>
2017-07-06 14:39:44 +0800

    文字修改

--- src/instock/cargo-flow/index.ts
+++ src/instock/cargo-flow/index.ts
@@ -23,7 +23,7 @@ export class CargoFlow {
       { route: '/separate/:id/new', name: 'separate', moduleId: './separate/new', title: "拆单" },
       { route: '/verify/:id', name: 'verify', moduleId: './verify/index', title: "库场审核" },
       { route: '/:id/change-historys', name: 'changeHistory', moduleId: './change-history/index', title: "修改记录" },
-      { route: '/work-order/:id', name: 'work-order', moduleId: './work-order/index', title: "添加作业" }
+      { route: '/work-order/:id', name: 'work-order', moduleId: './work-order/index', title: "作业列表" }
     ]);
     this.router = router;
 
