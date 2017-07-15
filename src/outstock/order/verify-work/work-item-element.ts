5c50f24c961d383ee1ac22cd6425f75e651161a6
sunshun <ssnoodles0226@gmail.com>
2017-07-15 13:25:25 +0800

    库场审核

---
+++ src/outstock/order/verify-work/work-item-element.ts
@@ -0,0 +1,25 @@
+import { DataSourceFactory } from "@app/utils";
+import { observable, inject} from "aurelia-framework";
+import { bindable, customElement } from "aurelia-templating";
+import { WorkOderItemService } from '@app/instock/services/work-order';
+
+@customElement('work-items')
+export class WorkItems {
+
+  @bindable
+  @observable
+  areaId: string;
+  dataSource: kendo.data.DataSource;
+
+  constructor(@inject private workOrderItemService: WorkOderItemService,
+              @inject private dataSourceFactory: DataSourceFactory) {
+  }
+  /**
+   * 加载数据
+   */
+  async bind() {
+    this.dataSource = this.dataSourceFactory.create({
+      readAll: () => this.workOrderItemService.getWorkOrderItemList(this.areaId)
+    });
+  }
+}
