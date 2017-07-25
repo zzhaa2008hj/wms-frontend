2e5a99f6a6feee8c013c438bc5912c73bd8ee2de
sunshun <ssnoodles0226@gmail.com>
2017-07-25 16:57:22 +0800

    1.修改库存页面 2.合并库存model

--- src/base/services/storage.ts
+++ src/base/services/storage.ts
@@ -1,8 +1,7 @@
 /**
  * Created by shun on 2017/6/26.
  */
-import {StorageInfo} from "@app/base/models/storage-info";
-import {StorageItem, StorageItemHistory} from "@app/base/models/storage-item";
+import {StorageInfoItem, StorageItemHistory, StorageInfo} from "@app/base/models/storage";
 import {StorageHistory} from "@app/base/models/storage-history";
 import {Query, handleResult, RestClient, fixDate} from "@app/utils";
 import {autoinject} from "aurelia-dependency-injection";
@@ -23,8 +22,8 @@ export class StorageService {
    * 查询库存明细
    * id 库存
    */
-  queryStorageItemPage(id: string, keywords?: string): Query<StorageItem> {
-    return this.http.query<StorageItem>(`/base/storage/${id}/item/page`, {keywords})
+  queryStorageItemPage(id: string, warehouseId?: string): Query<StorageInfoItem> {
+    return this.http.query<StorageInfoItem>(`/base/storage/${id}/item/page`, {warehouseId})
     .map(item => fixDate(item, 'instockDate'));
   }
 
@@ -41,5 +40,12 @@ export class StorageService {
    */
   async saveItem(id: string, storageItem: StorageItemHistory): Promise<void> {
     await this.http.post(`/base/storage/${id}/item`, storageItem).then(handleResult);
+  }
+  
+  /**
+   * 获取单条库存信息
+   */
+  getStorageInfoById(id: string): Promise<StorageInfo> {
+    return this.http.get(`/base/storage/${id}`).then(res => res.content);
   }
 }
