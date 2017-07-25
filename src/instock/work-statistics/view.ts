9126f467bc7ce79e86816e7bd1b84b0b4f4aeb60
谢余锋 <576562848@qq.com>
2017-07-25 20:55:43 +0800

    删除无效引用

--- src/instock/work-statistics/view.ts
+++ src/instock/work-statistics/view.ts
@@ -1,13 +1,12 @@
 import { autoinject } from "aurelia-dependency-injection";
 import { WorkOrder, WorkStatistics } from "@app/instock/models/work";
 import { WorkStatisticsService } from "@app/instock/services/work-statistics";
-import { WorkOrderItemService, WorkOrderService } from "@app/instock/services/work-order";
+import { WorkOrderItemService } from "@app/instock/services/work-order";
 import { CargoFlowService } from "@app/instock/services/cargo-flow";
 import { CargoFlow } from "@app/instock/models/cargo-flow";
 import { ConstantValues } from '@app/common/models/constant-values';
 import { DictionaryDataService } from '@app/base/services/dictionary';
 import { DictionaryData } from '@app/base/models/dictionary';
-import { WorkOrderDetail } from '@app/instock/models/work'
 
 @autoinject
 export class ViewWorkStatistics {
@@ -20,52 +19,39 @@ export class ViewWorkStatistics {
   units = [] as DictionaryData[];
 
   constructor(private workStatisticsService: WorkStatisticsService,
-              private workOrderService: WorkOrderService,
               private workOrderItemService: WorkOrderItemService,
               private cargoFlowService: CargoFlowService,
               private dictionaryDataService: DictionaryDataService) {
-    // this.datasource = new kendo.data.DataSource({
-    //   transport: {
-    //     read: (options) => {
-    //       options.success(this.listWorkOrders);
-    //     }
-    //   },
-    //   schema: {
-    //     model: {
-    //       id: 'id'
-    //     }
-    //   }
-    // });
-     
+
   }
 
-  
+
 
   async activate({ id }) {
     this.units = await this.dictionaryDataService.getDictionaryDatas('unit');
     this.workStatistics = await this.workStatisticsService.getWorkStatisticsById(id);
-    if(this.workStatistics.unit){
+    if (this.workStatistics.unit) {
       this.workStatistics.unit = this.units.find(r => r.dictDataCode == this.workStatistics.unit).dictDataName;
     }
     /*通过统计表的业务主键查询 流水信息*/
     this.cargoFlow = await this.cargoFlowService.getCargoFlowById(this.workStatistics.businessId);
 
     this.datasource = new kendo.data.DataSource({
-          transport: {
-            read: options => {
-              this.workOrderItemService.getWorkDetails(this.cargoFlow.id)
-                .then(res =>{
-                  res.map(e =>{
-                    if(e.unit){
-                      e.unit = this.units.find(r => r.dictDataCode == e.unit).dictDataName;
-                    };
-                  })
-                  options.success(res);
-                })
-                .catch(err => options.error("", "", err));
-            }
-          }
-      });
+      transport: {
+        read: options => {
+          this.workOrderItemService.getWorkDetails(this.cargoFlow.id)
+            .then(res => {
+              res.map(e => {
+                if (e.unit) {
+                  e.unit = this.units.find(r => r.dictDataCode == e.unit).dictDataName;
+                };
+              })
+              options.success(res);
+            })
+            .catch(err => options.error("", "", err));
+        }
+      }
+    });
   }
 
   formatCategory(category: number) {
