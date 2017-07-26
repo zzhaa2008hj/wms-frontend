23acbafb4f83bbf369ec35c836cdc502ffdad921
Hui <792717352@qq.com>
2017-07-26 17:01:56 +0800

    车辆信息验证;出库货物计算

--- src/instock/cargo-flow/vehicle/new.ts
+++ src/instock/cargo-flow/vehicle/new.ts
@@ -1,8 +1,9 @@
 import { autoinject, Container } from "aurelia-dependency-injection";
 import { DialogController } from "ui";
 import { Vehicle } from "@app/instock/models/cargo-flow";
-import { ValidationController, ValidationControllerFactory, ValidationRules } from 'aurelia-validation';
+import { ValidationController, ValidationControllerFactory } from 'aurelia-validation';
 import { formValidationRenderer } from "@app/validation/support";
+import { vehicleValidationRules } from "@app/outstock/models/order";
 /**
  * Created by Hui on 2017/6/14.
  */
@@ -28,7 +29,7 @@ export class NewVehicle {
   }
 
   async save() {
-    this.validationController.addObject(this.vehicle, validationRules);
+    this.validationController.addObject(this.vehicle, vehicleValidationRules);
     let { valid } = await this.validationController.validate();
     if (!valid) return;
     await this.dialogController.ok(this.vehicle);
@@ -38,25 +39,3 @@ export class NewVehicle {
     await this.dialogController.cancel();
   }
 }
-
-const validationRules = ValidationRules
-  .ensure((vehicle: Vehicle) => vehicle.plateNumber)
-  .displayName('车牌号')
-  .required().withMessage(`\${$displayName} 不能为空`)
-
-  .ensure((vehicle: Vehicle) => vehicle.driverName)
-  .displayName('司机名称')
-  .required().withMessage(`\${$displayName} 不能为空`)
-
-  .ensure((vehicle: Vehicle) => vehicle.driverIdentityNumber)
-  .displayName('身份证号')
-  .required().withMessage(`\${$displayName} 不能为空`)
-
-  .ensure((vehicle: Vehicle) => vehicle.phoneNumber)
-  .displayName('电话')
-  .required().withMessage(`\${$displayName} 不能为空`)
-
-  .ensure((vehicle: Vehicle) => vehicle.remark)
-  .displayName('备注')
-  .maxLength(500).withMessage(`\${$displayName} 过长`)
-  .rules;
--- src/outstock/models/order.ts
+++ src/outstock/models/order.ts
@@ -1,3 +1,4 @@
+import { ValidationRules } from 'aurelia-validation';
 export interface Order {
   id: string;
   cargoInfoId: string;
@@ -96,3 +97,53 @@ export interface Vehicle {
   orgId: string;
 }
 
+export const orderValidationRules = ValidationRules
+  .ensure((order: Order) => order.contactPerson)
+  .displayName('联系人')
+  .required().withMessage(`\${$displayName} 不能为空`)
+
+  .ensure((order: Order) => order.batchNumber)
+  .required().withMessage(`请选择批次`)
+
+  .ensure((order: Order) => order.paymentUnit)
+  .displayName('付款单位')
+  .required().withMessage(`\${$displayName} 不能为空`)
+
+  .ensure((order: Order) => order.contactNumber)
+  .displayName('联系电话')
+  .required().withMessage(`\${$displayName} 不能为空`)
+  .satisfies(x => /^[1][34578][0-9]{9}$/.test(x)).withMessage(` 请输入正确的11位手机号码 e.g.139 0000 0000`)
+
+  .ensure((order: Order) => order.takeDeliveryNum)
+  .displayName('提货单号')
+  .required().withMessage(`\${$displayName} 不能为空`)
+
+  .ensure((order: Order) => order.remark)
+  .displayName('备注')
+  .maxLength(500).withMessage(`\${$displayName} 过长`)
+  .rules;
+
+export const vehicleValidationRules = ValidationRules
+  .ensure((vehicle: Vehicle) => vehicle.plateNumber)
+  .displayName('车牌号')
+  .required().withMessage(`\${$displayName} 不能为空`)
+  .satisfies(x => /^([\u4e00-\u9fa5][a-zA-Z](([DF](?![a-zA-Z0-9]*[IO])[0-9]{4,5})|([0-9]{5}[DF])))|([冀豫云辽黑湘皖鲁苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼渝京津沪新京军空海北沈兰济南广成使领A-Z]{1}[a-zA-Z0-9]{5,6}[a-zA-Z0-9挂学警港澳]{1})$/
+    .test(x)).withMessage(` 请输入正确车牌号`)
+
+  .ensure((vehicle: Vehicle) => vehicle.driverName)
+  .displayName('司机名称')
+  .required().withMessage(`\${$displayName} 不能为空`)
+  .satisfies(x => /^([\u4e00-\u9fa5]{1,20}|[a-zA-Z\.\s]{1,20})$/.test(x)).withMessage(` 请输入正确的姓名`)
+
+  .ensure((vehicle: Vehicle) => vehicle.driverIdentityNumber)
+  .displayName('身份证号')
+  .required().withMessage(`\${$displayName} 不能为空`)
+  .satisfies(x => /^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$/.test(x)
+                  || /^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/.test(x))
+                    .withMessage(` 请输入正确的15/18位身份证号`)
+
+  .ensure((vehicle: Vehicle) => vehicle.phoneNumber)
+  .displayName('电话')
+  .required().withMessage(`\${$displayName} 不能为空`)
+  .satisfies(x => /^[1][34578][0-9]{9}$/.test(x)).withMessage(` 请输入正确的11位手机号码 e.g.139 0000 0000`)
+  .rules;
