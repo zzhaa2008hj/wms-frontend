b56b3ded1a2c657a18dcde3b31ba259937d94d78
Hui <792717352@qq.com>
2017-07-27 08:47:48 +0800

    Merge branch 'master' of https://git.oschina.net/mushang/wms-frontend into hui
    # Conflicts:
    #	src/base/storage/items/new.html

--- src/outstock/order/list.ts
+++ src/outstock/order/list.ts
@@ -195,15 +195,12 @@ export class OrderList {
         return;
       }
       // 跳转 到出库单页面
-      this.goStockOut();
+      this.router.navigateToRoute('outstock-orderItem');
     } catch (err) {
       await this.messageDialogService.alert({ title: "提示", message: err.message, icon: "error" });
     }
   }
 
-  goStockOut() {
-    this.router.navigateToRoute('outstock-orderItem');
-  }
 
   /**
    * 审核记录
