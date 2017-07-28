52582c74e8f6c0ed52710df0d427ae8a5d343fe9
xiao <zhangxiaojin11@163.com>
2017-07-28 16:12:55 +0800

    合同编辑gird一些字段变为不能修改

--- src/base/contract/new.ts
+++ src/base/contract/new.ts
@@ -83,13 +83,14 @@ export class NewContract {
             },
             chargeCategory: { editable: false },
             chargeType: { editable: false },
-            unit: { editable: false },
+            unitStr: { editable: false },
+            rateTypeStr: { editable: false },
             pricingMode: { editable: false },
             workName: { editable: false },
-            warehouseType: { editable: false },
+            warehouseTypeStr: { editable: false },
             cargoCategoryName: { editable: false },
             cargoSubCategoryName: { editable: false },
-            warehouseCategory: { editable: false },
+            warehouseCategoryStr: { editable: false },
             remark: { editable: false }
           }
         }
