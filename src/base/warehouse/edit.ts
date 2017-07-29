adc2bd8fc571541a1de76e7a7c6b4311cecade9c
谢余锋 <576562848@qq.com>
2017-07-29 14:15:23 +0800

    细节修改

--- src/base/warehouse/edit.ts
+++ src/base/warehouse/edit.ts
@@ -1,17 +1,29 @@
+import { observable } from 'aurelia-binding';
 import { autoinject, Container } from "aurelia-dependency-injection";
-import { DialogController } from "ui";
+import { DialogController, MessageDialogService } from 'ui';
 import { Warehouse } from "@app/base/models/warehouse";
 import { ValidationController, ValidationControllerFactory, ValidationRules } from 'aurelia-validation';
 import { formValidationRenderer } from "@app/validation/support";
+import { DictionaryDataService } from '@app/base/services/dictionary';
+import { DictionaryData } from '@app/base/models/dictionary';
 /**
  * Created by Hui on 2017/6/14.
  */
 @autoinject
 export class EditWarehouse {
+  oldWarehouse = {} as Warehouse;
   warehouse: Warehouse = {} as Warehouse;
+  @observable
+  hasParentId: boolean = false;
+  type = [] as DictionaryData[];
+  category = [] as DictionaryData[];
   validationController: ValidationController;
+
   constructor(private dialogController: DialogController,
-              validationControllerFactory: ValidationControllerFactory, container: Container) {
+              private messageDialogService: MessageDialogService,
+              private dictionaryDataService: DictionaryDataService,
+              validationControllerFactory: ValidationControllerFactory,
+              container: Container) {
     this.validationController = validationControllerFactory.create();
     this.validationController.addRenderer(formValidationRenderer);
     container.registerInstance(ValidationController, this.validationController);
@@ -19,7 +31,13 @@ export class EditWarehouse {
   }
 
   async activate(warehouse: Warehouse) {
+    this.type = await this.dictionaryDataService.getDictionaryDatas("warehouseType");
+    this.category = await this.dictionaryDataService.getDictionaryDatas("warehouseCategory");
+    Object.assign(this.oldWarehouse, warehouse);
     this.warehouse = warehouse;
+    if (this.warehouse.parentId) {
+      this.hasParentId = true;
+    }
   }
 
   async save() {
@@ -27,7 +45,16 @@ export class EditWarehouse {
     let { valid } = await this.validationController.validate();
     if (!valid) return;
 
-    await this.dialogController.ok(this.warehouse);
+    if (this.oldWarehouse.category != this.warehouse.category || this.oldWarehouse.type != this.warehouse.type) {
+      let confirmed = await this.messageDialogService.confirm({ title: "提示:", message: "修改后子集性质与类别将同步修改，确认吗？" });
+      if (confirmed) {
+        await this.dialogController.ok(this.warehouse);
+      } else {
+        return;
+      }
+    } else {
+      await this.dialogController.ok(this.warehouse);
+    }
   }
 
   async cancel() {
