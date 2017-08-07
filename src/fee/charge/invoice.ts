05f785d728e6b33c2df3542c6b62bdfb23fe0872
hui <792717352@qq.com>
2017-08-07 13:43:37 +0800

    收费 发票号录入验证

--- src/fee/charge/invoice.ts
+++ src/fee/charge/invoice.ts
@@ -1,17 +1,29 @@
 import { DialogController } from "ui";
 import { autoinject } from "aurelia-dependency-injection";
-import { Invoice } from "@app/fee/models/invoice";
+import { ChargeAuditList, chargeAuditListValidationRules } from "@app/fee/models/charge-audit";
+import { ValidationController, ValidationControllerFactory } from "aurelia-validation";
+import { formValidationRenderer } from "@app/validation/support";
 
 @autoinject
 export class InvoiceEntry {
-  invoice = {} as Invoice;
+  chargeAuditList = {} as ChargeAuditList;
 
-  constructor(private dialogController: DialogController) {
-    this.invoice.invoiceType = 1;
+  constructor(private dialogController: DialogController,
+              private validationControllerFactory: ValidationControllerFactory) {
+    this.chargeAuditList.invoiceType = 1;
+    this.validationController = this.validationControllerFactory.create();
+    this.validationController.addRenderer(formValidationRenderer);
+    this.container.registerInstance(ValidationController, this.validationController);
+  }
+
+  activate() {
+    this.validationController.addObject(this.paymentAuditList, chargeAuditListValidationRules);
   }
 
   async save() {
-    await this.dialogController.ok(this.invoice);
+    let { valid } = await this.validationController.validate();
+    if (!valid) return;
+    await this.dialogController.ok(this.chargeAuditList);
   }
 
   async cancel() {
