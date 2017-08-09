bcafa7c9bb33448b463f1c53658fb2ff98d3ab10
hui <792717352@qq.com>
2017-08-09 09:03:43 +0800

    出库 出库清单 页面修改,打印方法完善;出库单页面恢复

--- src/outstock/order/item/view.ts
+++ src/outstock/order/item/view.ts
@@ -4,13 +4,11 @@ import { Order } from "@app/outstock/models/order";
 import { OrderService } from "@app/outstock/services/order";
 import { DictionaryData } from "@app/base/models/dictionary";
 import { DictionaryDataService } from "@app/base/services/dictionary";
-import { addHeader,print } from "@app/common/services/print-tool";
-import * as moment from 'moment';
-
 @autoinject
 export class OrderView {
   units = [] as DictionaryData[];
   order = {} as Order;
+  datasource: kendo.data.DataSource;
 
   constructor(private router: Router,
               private dictionaryDataService: DictionaryDataService,
@@ -24,27 +22,27 @@ export class OrderView {
     this.units = await this.dictionaryDataService.getDictionaryDatas("unit");
     this.order = await this.orderService.getOutstockOrderView(id);
 
-    this.order.createTimeStr = moment(this.order.createTime).format("YYYY-MM-DD");
-
     if (this.order.unitStr) this.order.unitStr = this.units.find(r => r.dictDataCode == this.order.unit).dictDataName;
     this.order.outstockOrderItems.map(res => {
       res.unitStr = this.units.find(r => r.dictDataCode == res.unit).dictDataName;
-      Object.assign(res, { index: this.order.outstockOrderItems.indexOf(res) + 1 });
       return res;
+    });
+
+    this.datasource = new kendo.data.DataSource({
+      transport: {
+        read: (options) => {
+          options.success(this.order.outstockOrderItems);
+        }
+      },
+      schema: {
+        model: {
+          id: 'id'
+        }
+      }
     });
   }
 
   cancel() {
     this.router.navigateToRoute("list");
-  }
-
-  /**
-   * 打印出库单
-   */
-  printOutStockItem(){
-    let title = "出库单";
-    let strHTML = $("#outStockItem").html();
-    strHTML = addHeader(strHTML);
-    print(title, strHTML, true);
   }
 }
