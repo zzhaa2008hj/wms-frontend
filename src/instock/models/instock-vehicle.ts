f4050877ec3acbe5a197f30d815f01ce781a6504
hui <792717352@qq.com>
2017-08-14 15:13:31 +0800

    Merge branches 'hui-dev' and 'master' of https://git.oschina.net/mushang/wms-server into hui-dev
    # Conflicts:
    #	src/main/java/com/eupwood/wms/instock/service/CargoFlowService.java

--- src/instock/models/instock-vehicle.ts
+++ src/instock/models/instock-vehicle.ts
@@ -1,5 +1,3 @@
-import { ValidationRules } from 'aurelia-validation';
-
 export interface InstockVehicle {
   id: string ;
 
@@ -22,25 +20,3 @@ export interface InstockVehicle {
 
   cargoName: string;
 }
-
-export const vehicleValidationRules = ValidationRules
-  .ensure((vehicle: InstockVehicle) => vehicle.plateNumber)
-  .displayName('车牌号')
-  .required().withMessage(`\${$displayName} 不能为空`)
-
-  .ensure((vehicle: InstockVehicle) => vehicle.driverName)
-  .displayName('司机名称')
-  .required().withMessage(`\${$displayName} 不能为空`)
-
-  .ensure((vehicle: InstockVehicle) => vehicle.driverIdentityNumber)
-  .displayName('身份证号')
-  .required().withMessage(`\${$displayName} 不能为空`)
-
-  .ensure((vehicle: InstockVehicle) => vehicle.phoneNumber)
-  .displayName('电话')
-  .required().withMessage(`\${$displayName} 不能为空`)
-
-  .ensure((vehicle: InstockVehicle) => vehicle.remark)
-  .displayName('备注')
-  .maxLength(500).withMessage(`\${$displayName} 过长`)
-  .rules;
