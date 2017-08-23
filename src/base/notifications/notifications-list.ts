3984925fa0c0a22118c1643220e38ce2a4ed273d
谢余锋 <576562848@qq.com>
2017-08-23 14:23:12 +0800

    代码格式化

--- src/base/notifications/notifications-list.ts
+++ src/base/notifications/notifications-list.ts
@@ -8,60 +8,60 @@ import { DialogService, MessageDialogService } from "ui";
 @autoinject
 export class MsgInfo {
 
-    pageable = {
-        refresh: true,
-        pageSizes: true,
-        buttonCount: 10
-    };
-    unreadDataSource: kendo.data.DataSource;
-    allDataSource: kendo.data.DataSource;
-    noteDataSource: kendo.data.DataSource;
+  pageable = {
+    refresh: true,
+    pageSizes: true,
+    buttonCount: 10
+  };
+  unreadDataSource: kendo.data.DataSource;
+  allDataSource: kendo.data.DataSource;
+  noteDataSource: kendo.data.DataSource;
 
-    constructor(private dataSourceFactory: DataSourceFactory,
-                private messageService: MessageService,
-                private messageResultService : MessageResultService,
-                private noticeService: NoticeService,
-                private dialogService : DialogService,
-                private messageDialogService : MessageDialogService
-                ) {
+  constructor(private dataSourceFactory: DataSourceFactory,
+              private messageService: MessageService,
+              private messageResultService: MessageResultService,
+              private noticeService: NoticeService,
+              private dialogService: DialogService,
+              private messageDialogService: MessageDialogService) {
 
-        this.unreadDataSource = this.dataSourceFactory.create({
-            query: () => this.messageService.getMsgInfo({read:"1"}),
-            pageSize: 15
-        });
-        this.allDataSource = this.dataSourceFactory.create({
-            query: () => this.messageService.getMsgInfo({read: "0"}),
-            pageSize: 15
-        });
+    this.unreadDataSource = this.dataSourceFactory.create({
+      query: () => this.messageService.getMsgInfo({ read: "1" }),
+      pageSize: 15
+    });
+    this.allDataSource = this.dataSourceFactory.create({
+      query: () => this.messageService.getMsgInfo({ read: "0" }),
+      pageSize: 15
+    });
 
 
-        this.noteDataSource = this.dataSourceFactory.create({
-            query: () => this.noticeService.queryNotices({ visible: "1" }),
-            pageSize: 15
-        });
+    this.noteDataSource = this.dataSourceFactory.create({
+      query: () => this.noticeService.queryNotices({ visible: "1" }),
+      pageSize: 15
+    });
+  }
+
+  async detail(obj) {
+    this.messageResultService.updateMessage(obj.id);
+    await this.dialogService
+      .open({ viewModel: ReadNotification, model: { body: obj.content, title: obj.title }, lock: true })
+      .whenClosed();
+  }
+
+  async detailNote(obj) {
+    await this.dialogService
+      .open({ viewModel: ReadNotification, model: { body: obj.content, title: obj.title }, lock: true })
+      .whenClosed();
+  }
+
+  async setRead() {
+    try {
+      await this.messageResultService.setRead();
+      await this.messageDialogService.alert({ title: "设置成功" });
+      this.unreadDataSource.read();
+      this.allDataSource.read();
+    } catch (err) {
+      await this.messageDialogService.alert({ title: "发生错误", message: err.message, icon: 'error' });
     }
-
-    async activate(){
-    }
-    async detail(obj){
-        this.messageResultService.updateMessage(obj.id);
-        await this.dialogService.open({viewModel: ReadNotification ,model:{body: obj.content,title: obj.title},lock: true}).whenClosed();
-    }
-
-    async detailNote(obj){
-        await this.dialogService.open({viewModel: ReadNotification ,model:{body: obj.content,title: obj.title},lock: true}).whenClosed();
-    }
-
-    async setRead(){
-        try {
-            await this.messageResultService.setRead();
-            await this.messageDialogService.alert({ title: "设置成功" });
-            this.unreadDataSource.read();
-            this.allDataSource.read();
-        } catch (err) {
-            await this.messageDialogService.alert({ title: "发生错误", message: err.message, icon: 'error' });
-        }
-
-    }
+  }
 
 }
