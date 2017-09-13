d0e8d6ab083c15e5c126fe69c02d50197fe68dd1
Emily <shenye321@126.com>
2017-09-13 09:36:30 +0800

    2.仓库库区中子库区的库区性质可以和父库区不一样

--- src/base/models/warehouse.ts
+++ src/base/models/warehouse.ts
@@ -1,6 +1,7 @@
 /**
  * Created by Hui on 2017/6/15.
  */
+import { ValidationRules } from 'aurelia-validation';
 export interface Warehouse {
   id?: string;
   parentId?: string;
@@ -15,3 +16,22 @@ export interface Warehouse {
   typeStr: string;
   categoryStr: string;
 }
+
+export const wareHouseValidationRules = ValidationRules
+  .ensure((warehouse: Warehouse) => warehouse.name)
+  .displayName('库区名称')
+  .required().withMessage(`\${$displayName} 不能为空`)
+
+  .ensure((warehouse: Warehouse) => warehouse.type)
+  .displayName('库区性质')
+  .required().withMessage(`\${$displayName} 不能为空`)
+
+  .ensure((warehouse: Warehouse) => warehouse.category)
+  .displayName('库区类别')
+  .required().withMessage(`\${$displayName} 不能为空`)
+
+
+  .ensure((warehouse: Warehouse) => warehouse.remark)
+  .displayName('备注')
+  .maxLength(500).withMessage(`\${$displayName} 过长`)
+  .rules;
