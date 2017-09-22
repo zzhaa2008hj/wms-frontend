751e5f283df5753ea925e8421cfc006e1d98df7c
曹亮亮 <1023607383@qq.com>
2017-09-22 13:34:57 +0800

    货位转移 撤回操作

--- src/cargo-position/info/list.ts
+++ src/cargo-position/info/list.ts
@@ -2,6 +2,10 @@ import { autoinject } from 'aurelia-dependency-injection';
 import { DataSourceFactory } from '@app/utils';
 import { DictionaryData } from "@app/base/models/dictionary";
 import { StatisticsCriteria, PositionTransferInfoService } from "@app/cargo-position/services/transfer-info";
+import { VerifyRecord } from '@app/common/models/verify-record';
+import { DialogService, MessageDialogService } from 'ui';
+import { NewVerifyRecord } from '@app/common/verify-records/new';
+import { VerifyRecordService } from '@app/common/services/verify-record';
 
 @autoinject
 export class PositionTransferInfoList {
@@ -17,7 +21,10 @@ export class PositionTransferInfoList {
   };
 
   constructor(private positionTransferInfoService: PositionTransferInfoService,
-              private dataSourceFactory: DataSourceFactory) {
+              private dataSourceFactory: DataSourceFactory,
+              private dialogService: DialogService,
+              private messageDialogService: MessageDialogService,
+              private verifyRecordService: VerifyRecordService) {
   }
 
   async activate() {
@@ -34,6 +41,32 @@ export class PositionTransferInfoList {
   reset() {
     this.criteria = {};
     this.dataSource.read();
+  }
+
+  //撤回
+  async rollBack(item) {
+    let verifyRecord: VerifyRecord = {} as VerifyRecord;
+    verifyRecord.businessId = item.id;
+    verifyRecord.businessName = item.transferNumber;
+    verifyRecord.businessType = 4;
+    verifyRecord.stageBeforeVerify = item.stage;
+    verifyRecord.batchNumber = item.batchNumber;
+    let result = await this.dialogService
+      .open({ viewModel: NewVerifyRecord, model: verifyRecord, lock: true })
+      .whenClosed();
+    if (result.wasCancelled) return;
+    let res = result.output;
+    try {
+      let conformed = await this.messageDialogService.confirm({ title: "撤回成功", message: "确认提交撤回的审批请求？" });
+      if (!conformed) {
+        return;
+      }
+      await this.verifyRecordService.addVerifyRecord(res);
+      await this.messageDialogService.alert({ title: "撤回成功", message: "撤回成功！" });
+      this.dataSource.read();
+    } catch (err) {
+      await this.messageDialogService.alert({ title: "撤回失败", message: err.message, icon: "error" });
+    }
   }
 
 }
