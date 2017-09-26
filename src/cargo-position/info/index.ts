fc43acf6ef321d0260660f6f5f32e9a2e81cac66
hui <792717352@qq.com>
2017-09-26 16:54:03 +0800

    货位转移修改

--- src/cargo-position/info/index.ts
+++ src/cargo-position/info/index.ts
@@ -7,6 +7,7 @@ export class PositionTransferInfo {
     config.map([
       { route: '', name: 'list', moduleId: './list' },
       { route: '/new', name: 'new', moduleId: './new', title: '新增' },
+      { route: '/:id/edit', name: 'edit', moduleId: './edit', title: '修改' },
       { route: '/:id/view', name: 'view', moduleId: './view', title: '查看' }
     ]);
     this.router = router;
