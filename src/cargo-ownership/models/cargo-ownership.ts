b73a0e7ae8d74936a4e38ba4d18d246ef8183d62
sunshun <ssnoodles0226@gmail.com>
2017-10-09 14:29:31 +0800

    修改货权转移费率新增方式

--- src/cargo-ownership/models/cargo-ownership.ts
+++ src/cargo-ownership/models/cargo-ownership.ts
@@ -100,33 +100,6 @@ export interface CargoownershipTransferItem {
   cargoCategoryId: string;
 
   cargoCategoryName: string;
-  //品牌名称
-
-  transferItems: TransferCargoItemVo[];
-  attachments: AttachmentMap[];
-  // 0 生成新的批次 1并票
-  batchNumberMode: number;
-
-  storageEndDateStr: string;
-  transferDateStr: string;
-}
-
-export interface CargoownershipTransferItem {
-  id: string;
-  orgId: string;
-  remark: string;
-
-  batchNumber: string;
-  // 货权转移ID
-  cargoownershipTransferId: string;
-
-  cargoItemId: string;
-
-  cargoName: string;
-
-  cargoCategoryId: string;
-
-  cargoCategoryName: string;
 
   cargoSubCategoryName: string;
   // 指令数量
@@ -226,43 +199,8 @@ export interface TransferCargoItemVo {
   storageItems: TransferStorageItemVo[];
   
   unitName: string;
-}
-export interface TransferCargoItemVo {
-  id: string;
-  orgId: string;
-  remark: string;
-  cargoInfoId: string;
-  batchNumber: string;
-  cargoName: string;
-  cargoCategoryId: string;
-  cargoCategoryName: string;
-  cargoSubCatergoryName: string;
-  unit: string;
-  freeDays: number;
-
-  // 库存
-  storageQuantity: number;
-  storageNumber: number;
-  // 费收扣量
-  distrainQuantity: number;
-  distrainNumber: number;
-  // 正在出库量
-  outstockQuantity: number;
-  outstockNumber: number;
-  // 实际可转移数
-  quantity: number;
-  number: number;
-  // 转移数
-  transferQuantity: number;
-  transferNumber: number;
-
-  transferFreeDays: number;
-  // 费率
-  cargoRates: CargoRate[];
-  // 库存明细
-  storageItems: TransferStorageItemVo[];
-  
-  unitName: string;
+  // 合同费率
+  contractRates: CargoRate[];
 }
 
 export interface TransferStorageItemVo extends StorageItem{
@@ -298,128 +236,6 @@ export interface CargoOwnershipTranferRateStep {
 
   canQuantity: number;
   canNumber: number;
-}
-export interface TransferCargoStorageItem{
-  id: string;
-  // 库存信息表主键
-  transferItemId: string;
-  // 库区主键
-  warehouseId: string;
-  // 库区名称
-  warehouseName: string;
-  // 集装箱号
-  containerNumber: string;
-  // 堆存数量
-  storageQuantity: number;
-  // 堆存件数
-  storageNumber: number;
-  // 计量单位
-  unit: string;
-
-  remark: string;
-
-  orgId: string;
-
-  unitName: string;
-
-  canQuantity: number;
-  canNumber: number;
-}
-export interface TransferCargoItemVo {
-  id: string;
-  orgId: string;
-  remark: string;
-  cargoInfoId: string;
-  batchNumber: string;
-  cargoName: string;
-  cargoCategoryId: string;
-  cargoCategoryName: string;
-  cargoSubCatergoryName: string;
-  unit: string;
-  freeDays: number;
-
-  // 库存
-  storageQuantity: number;
-  storageNumber: number;
-  // 费收扣量
-  distrainQuantity: number;
-  distrainNumber: number;
-  // 正在出库量
-  outstockQuantity: number;
-  outstockNumber: number;
-  // 实际可转移数
-  quantity: number;
-  number: number;
-  // 转移数
-  transferQuantity: number;
-  transferNumber: number;
-
-  transferFreeDays: number;
-  // 费率
-  cargoRates: CargoRate[];
-  // 库存明细
-  storageItems: TransferStorageItemVo[];
-  
-  unitName: string;
-}
-
-export interface TransferStorageItemVo extends StorageItem{
-  // 可转数量
-  quantity: number;
-  number: number;
-}
-
-export interface CargoownershipTransferVo {
-  id: string;
-  orgId: string;
-  remark: string;
-  // 客户货物主键
-  cargoInfoId: string;
-  // 原批次号
-  originalBatchNumber: string;
-  // 原代理商主键
-  originalAgentId: string;
-  // 原代理商名称
-  originalAgentName: string;
-  // 原客户主键
-  originalCustomerId: string;
-  // 原客户名称
-  originalCustomerName: string;
-  // 新批次号
-  newBatchNumber: string;
-  // 新代理商主键
-  newAgentId: string;
-  // 新代理商名称
-  newAgentName: string;
-  // 新客户主键
-  newCustomerId: string;
-  // 新客户名称
-  newCustomerName: string;
-  // 转移日期
-  transferDate: Date;
-  // 仓储费结算至日期
-  storageEndDate: Date;
-  // 出库费承担方主键
-  outstockChargePayerId: string;
-  // 出库费承担方名称
-  outstockChargePayerName: string;
-  // 业务状态
-  status: number;
-  // 上一阶段号
-  lastStage: number;
-  // 当前阶段号
-  stage: number;
-  // 审核人主键
-  auditorId: string;
-  // 审核人名称
-  auditorName: string;
-  // 审核时间
-  auditTime: Date;
-  stageName: string;
-  lastStageName: string;
-  transferDateStr: string;
-  storageEndDateStr: string;
-  transferItems: CargoownershipTransferItem[];
 }
 
 export interface CargoownershipTransferVo {
