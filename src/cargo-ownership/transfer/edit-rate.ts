06ed1d795eaee57b5d7e3b750bc7777728ee7751
sunshun <ssnoodles0226@gmail.com>
2017-10-09 16:28:56 +0800

    去除重复代码

--- src/cargo-ownership/transfer/edit-rate.ts
+++ src/cargo-ownership/transfer/edit-rate.ts
@@ -7,7 +7,6 @@ import { formValidationRenderer } from '@app/validation/support';
 import { DictionaryDataService } from '@app/base/services/dictionary';
 import { DictionaryData } from '@app/base/models/dictionary';
 import { ConstantValues } from '@app/common/models/constant-values';
-import { NewRate } from '@app/cargo-ownership/transfer/edit-new-rate';
 import { ContractSearch } from '@app/base/models/contract';
 
 @autoinject
